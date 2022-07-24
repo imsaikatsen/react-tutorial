8957b0cd38f09967f9ad83128dc8b414b9685ecd
imsaikatsen <sensaikat100@gmail.com>
2022-07-25 00:02:04 +0600

    class and functional component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-//functional component
-function clock() {
-  return (
-    <h1 className="heading">
-      <span className="text">Hello {new Date().toLocaleTimeString()}</span>
-    </h1>
-  );
+//class component
+
+class Clock extends React.Component{
+  render(){
+    return (
+      <h1 className="heading">
+        <span className="text">Hello {new Date().toLocaleTimeString(this.props.locale)}</span>
+      </h1>
+    );
+  }
 }
 
-ReactDOM.render(clock(), document.getElementById('root'));
+ReactDOM.render(<Clock locale="bn-BD" />, document.getElementById('root'));
