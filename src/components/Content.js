d38ec232635756fd7fd7452bedf5de8d8f23b3e5
imsaikatsen <sensaikatcse@gmail.com>
2022-11-27 16:28:32 +0600

    context another way with class component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,20 +1,25 @@
+import React from 'react';
 import Counter from './Counter';
 import HoverCounter from './HoverCounter';
 import ThemeContext from '../contexts/themeContext';
-export default function Content() {
+export default class Content extends React.Component{
+  componentDidMount(){
+    console.log(this.context)
+  }
+ render(){
+  const {theme, switchTheme} = this.context
   return (
     <div>
       <h1>This is Content</h1>
       <Counter>
         {(counter, incrementCount) => (
-          <ThemeContext.Consumer>
-            {({ theme, switchTheme }) => (
               <HoverCounter count={counter} incrementCount=
                 {incrementCount} theme={theme} switchTheme={switchTheme} />)}
-          </ThemeContext.Consumer>
-        )}
       </Counter>
     </div>
 
   )
+ }
 }
+
+Content.contextType = ThemeContext;
--- src/components/HoverCounter.js
+++ src/components/HoverCounter.js
@@ -1,7 +1,6 @@
 import React from "react";
 export default function HoverCounter ({count, incrementCount , theme, switchTheme}){
     const style = theme === 'dark' ? { backgroundColor: '#000000', color: '#ffffff' } : null; // created a object style naming
-    console.log('HoverCounter rendered');
         return (
             <div>
                 <h1 type="button" onMouseOver={incrementCount}  style={style}>Hovered {count} times</h1>
