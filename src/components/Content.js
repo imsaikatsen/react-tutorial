2892147c01110d9825809eeee101aefc91d19eba
imsaikatsen <sensaikatcse@gmail.com>
2022-11-27 16:36:12 +0600

    context with functional component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,25 +1,20 @@
-import React from 'react';
 import Counter from './Counter';
 import HoverCounter from './HoverCounter';
 import ThemeContext from '../contexts/themeContext';
-export default class Content extends React.Component{
-  componentDidMount(){
-    console.log(this.context)
-  }
- render(){
-  const {theme, switchTheme} = this.context
+import { useContext } from 'react';
+export default function Content() {
+  const context = useContext(ThemeContext);
+  const {theme,switchTheme} =  context; 
   return (
     <div>
       <h1>This is Content</h1>
       <Counter>
         {(counter, incrementCount) => (
-              <HoverCounter count={counter} incrementCount=
-                {incrementCount} theme={theme} switchTheme={switchTheme} />)}
+          <HoverCounter count={counter} incrementCount=
+          {incrementCount} theme={theme} switchTheme={switchTheme} />
+        )}
       </Counter>
     </div>
 
   )
- }
 }
-
-Content.contextType = ThemeContext;
