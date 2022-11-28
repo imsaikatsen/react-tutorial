72f9030df8595fa8b645a269171734f29cc51731
imsaikatsen <sensaikatcse@gmail.com>
2022-11-28 13:37:34 +0600

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 import React from 'react';
-import ClickCounter from './components/ClickCounter';
-import Counter from './components/Counter';
-import Section from './components/Section';
-import ThemeContext from './contexts/themeContext';
+import Todo from './components/Todo';
 export default class App extends React.Component {
-  state = {
-    theme: 'light',
-  }
-
-  switchTeme = () => {
-    this.setState(({theme}) => {
-      if(theme=== 'dark'){
-        return {
-          theme : 'light'
-        }
-      } else {
-        return {
-          theme : 'dark'
-        }
-      }
-    })
-  }
 
  render(){
-  const {theme} = this.state; //theme destracturing from state
   return (
     <div className="app"> 
-      <Counter>
-        {(counter,incrementCount) => (
-          <ClickCounter count={counter} incrementCount=
-          {incrementCount} />
-        )}
-      </Counter>
-      <ThemeContext.Provider value = {{ theme, switchTheme : this.switchTeme }}>
-      <Section />
-      </ThemeContext.Provider>
+      <Todo />
     </div>
   );
  }
